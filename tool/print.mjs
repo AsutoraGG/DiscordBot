a90f847539d82ad064cdefb64844463ac22df529
AsutoraGG <76235964+AsutoraGG@users.noreply.github.com>
2022-07-18 16:35:04 +0900

    更新

--- tool/print.mjs
+++ tool/print.mjs
@@ -1,13 +1,20 @@
-import chalk from 'chalk';
+import chalk, { Chalk } from 'chalk';
 import { readFile } from 'fs/promises';
 import RustPlus from "@liamcottle/rustplus.js";
 
 const C = JSON.parse(await readFile(new URL('../config.json', import.meta.url)));
 var rustplus = new RustPlus(C.rp.ip, C.rp.port, C.rp.id, C.rp.token);
-
+function UnixtoDate(unix) {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var ampm = hours >= 12 ? '午後' : '午前';
+    var hourmin = ampm + hours + ":" + minutes.substr(-2)
+    return hourmin;
+}
 
 rustplus.on('connected', () => {
-    rustplus.sendTeamMessage('connected!')
+    null;
 });
 
 if(C.rp.connect === true) {
@@ -23,21 +30,24 @@ if(C.rp.connect === true) {
 // cはRustplusを使用しゲーム内(チームチャット)にメッセージを送信するか、又dはcがtrueだった場合メッセージの内容を決める
 export default function print(a, b, c) {
     if(c === true) {
-            rustplus.sendTeamMessage(`[${a}] : ${b}`);
+            rustplus.sendTeamMessage((`[${UnixtoDate(Date.now())}] `) + `[${a}] : ${b}`);
             //console.log(chalk.blueBright.bold(`[INFO] `) + `: ゲーム内にメッセージを送信しました`);
     } else if(c === false) {
         if(a === 'INFO') {
-            console.log(chalk.blueBright.bold(`[${a}] `) + `: ${b}`);
+            console.log(chalk.black.bold(`[${UnixtoDate(Date.now())}] `) + chalk.blueBright.bold(`[${a}] `) + `: ${b}`);
         } else if(a === 'ERROR') {
-            console.log(chalk.red.bold(`[${a}] `) + `: ${b}`);
+            console.log(chalk.black.bold(`[${UnixtoDate(Date.now())}] `) + chalk.red.bold(`[${a}] `) + `: ${b}`);
         } else if(a === 'DISCORD') {
-            console.log(chalk.green.bold(`[${a}] `) + `: ${b}`);
+            console.log(chalk.black.bold(`[${UnixtoDate(Date.now())}] `) + chalk.green.bold(`[${a}] `) + `: ${b}`);
         } else if(a === 'SUCCESS') {
-            console.log(chalk.cyan.bold(`[${a}] `) + `: ${b}`);
+            console.log(chalk.black.bold(`[${UnixtoDate(Date.now())}] `) + chalk.cyan.bold(`[${a}] `) + `: ${b}`);
         } else if (a === 'NONE'){
-            console.log(chalk.green.italic(`${b}`));
-        } else {
-            console.log(`[${a}] ` + `: ${b}`);
+            console.log(chalk.green.underline(`${b}`));
+        } else if(a === 'RUST') {
+            console.log(chalk.black.bold(`[${UnixtoDate(Date.now())}]`) + chalk.yellow.bold(`[${a}] `) + `: ${b}`);
+        } 
+        else {
+            console.log(chalk.white.bold(`[${UnixtoDate(Date.now())}] `) + `[${a}] ` + `: ${b}`);
         }
     } else {
         console.log(chalk.red.bold(`[ERROR] `) + `: print(a,b,c)のcに文字が入力されていません`);
